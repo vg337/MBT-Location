30f8d96fabfd2bdaf7d5d16093752f8ab4acc81a
Zunoc <jorys.bensch@gmail.com>
2019-09-17 16:44:06 +0200

    Maj TestManager

--- src/test/java/Generic/TestManager.java
+++ src/test/java/Generic/TestManager.java
@@ -1,10 +1,13 @@
-package Outils;
+package Generic;
 
+import java.lang.reflect.Method;
 import java.net.URL;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeMethod;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
@@ -14,17 +17,23 @@ import io.appium.java_client.remote.MobileCapabilityType;
 public class TestManager {
 
 	AppiumDriver<MobileElement> driver;
+	private ThreadLocal<String> testName = new ThreadLocal<>();
 
-	@BeforeTest
+	@BeforeSuite
 	public void setup() {
-		
+
+		System.out.println("===============================================");
+		System.out.println("------------------Test start!------------------");
+		System.out.println("===============================================");
 		System.out.println("Ouverture de l'APK.");
+		
+		// Mise en place des paramètres nécessaire au lancement de l'app sous Appium.
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "ANDROID");
 		capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Device");
-		//TO DO : Changer le UDID en fonction de votre appareil.
+		// TO DO : Changer le UDID en fonction de votre appareil.
 		capabilities.setCapability("udid", "CLCDU18613003856");
-		//TO DO : Changer le numéro de version android en fonction de votre appareil.
+		// TO DO : Changer le numéro de version android en fonction de votre appareil.
 		capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9");
 		capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 60);
 		capabilities.setCapability("appPackage", "com.sogeti.inblue.findrive");
@@ -43,12 +52,24 @@ public class TestManager {
 			exp.printStackTrace();
 		}
 	}
+	
+	@BeforeMethod
+	public void beforeMethod(Method method){
+	   testName.set(method.getName());
+	}
 
-	@AfterTest
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("Le test " + testName.get() + " est terminé." );
+	}
 
+	@AfterSuite
 	public void teardown() {
 
 		driver.quit();
+		System.out.println("===============================================");
+		System.out.println("-----------------Test finish !-----------------");
+		System.out.println("===============================================");
 
 	}
 
