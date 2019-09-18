dec2ac8d5a33c2721f0eac2c70e787f65321a28c
marilou1301 <marilou.prevoteau@gmail.com>
2019-09-18 14:40:52 +0200

    rectif US1.java

--- src/test/java/TestSuite/US1.java
+++ src/test/java/TestSuite/US1.java
@@ -14,21 +14,17 @@ public class US1 extends TestSuite {
 0	Début
 1	Quelle action faire ? = Mot de passe oublié
 2	Renseigner nom, prenom, email pour réinitialisation de mdp
-3	Est-ce que le nom et/ou le prénom sont  connus  ? = Non
+3	Est-ce que le nom et le prénom et le mail sont  connus  ? = Non
 4	Pop up Contacter une agence ou annuler ?
 5	Annuler 
 6	Renseigner nom, prenom, email pour demander la réinitialisation de mdp
-7	Est-ce que le nom et le prénom sont  connus  ? = Oui
-8	Est-ce que le champ contient une adresse email enregistrée ?  = Non
-9	Zone encadrée en rouge  
-10	Renseigner email 
-11	Est-ce que le champ contient une adresse email enregistrée ? = Oui
-12	Envoi d'un mail de réinitialisation de mot de passe
-13		Est-ce que le mail a été reçu ? = Oui    							  HORS SYSTEME
-14		Cliquer sur le lien reçu 											  HORS SYSTEME
-15		Renseigner mail, et mot de passe , et confirmation du mot de passe 	  HORS SYSTEME
-16  Quelle action faire ? = Connexion			 
-15	Renseigner email et mot de passe          
+7	Est-ce que le nom et le prénom et l'émail sont  connus  ? = Oui
+8	Envoi d'un mail de réinitialisation de mot de passe
+9		Est-ce que le mail a été reçu ? = Oui    							  HORS SYSTEME
+10		Cliquer sur le lien reçu 											  HORS SYSTEME
+11		Renseigner mail, et mot de passe , et confirmation du mot de passe 	  HORS SYSTEME
+12  Quelle action faire ? = Connexion			 
+13	Renseigner email et mot de passe          
 
 	CAS de test 2 Connexion 
 	
@@ -42,7 +38,7 @@ public class US1 extends TestSuite {
 5	Renseigner email et mot de passe 
 6	Est-ce que le champ contient une adresse email enregistrée ?  = Oui
 7	Est-ce que le champ contient le mot de passe associé au compte ? = Oui
-8	Connexion réussie , page d'accueil  réservation affiché
+8	Connexion réussie , page d'accueil  réservation affichée
 
 	 
 	 */
