b357e8000403c804169f75ac00c30dadbedd9944
Zunoc <jorys.bensch@gmail.com>
2019-09-18 15:03:01 +0200

    Ajout des fichiers EcranConnexion et EcranMdpOublie .java.

---
+++ src/main/java/Pages/EcranConnexion.java
@@ -0,0 +1,15 @@
+package Pages;
+
+import org.openqa.selenium.By;
+
+public class EcranConnexion {
+	
+	
+	private By btnMdpOublie = By.xpath("//*[contains(text(), 'Oublié ?')]");
+	private By champEmail = By.xpath("//*[contains(@resource-id, 'login_edit_user_name')]");
+	private By champMdp = By.xpath("//*[contains(@resource-id, 'login_edit_password')]");
+	private By btnConnexion = By.xpath("//*[contains(text(), 'SE CONNECTER')]");
+	private By textEchecCo = By.xpath("//*[contains(text(), 'Échec de connexion)]");
+	
+	
+}
---
+++ src/main/java/Pages/EcranMdpOublie.java
@@ -0,0 +1,14 @@
+package Pages;
+
+import org.openqa.selenium.By;
+
+public class EcranMdpOublie {
+
+	private By champPrenom = By.xpath("//*[contains(@resource-id, 'first_name')]");
+	private By champNom = By.xpath("//*[contains(@resource-id, 'last_name')]");
+	private By champEmail = By.xpath("//*[contains(@resource-id, 'email_address')]");
+	private By btnEnvoyer = By.xpath("//*[contains(text(), 'ENVOYER')]");
+	private By popUp = By.xpath("//*[contains(@class, 'FrameLayout')]");
+	private By btnAnnuler = By.xpath("//*[contains(text(), 'ANNULER')]");
+
+}
