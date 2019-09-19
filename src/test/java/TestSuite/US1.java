60fadb8ed25e4eb0986e106de7c78019c45bf5f6
Eldakhan <julien.armentia@hotmail.fr>
2019-09-19 11:18:27 +0200

    US1 CdT1 update

--- src/test/java/TestSuite/US1.java
+++ src/test/java/TestSuite/US1.java
@@ -52,26 +52,3 @@ public class US1 extends TestSuite {
 		
 	}
 }
-
-/*
-	
-Cas de test 1 : Mot de passe oublié 
-
-0	Début
-	Lancer l'application = la page FirstPage est affichée
-	Cliquer sur Se Connecter
-1	Quelle action faire ? = Mot de passe oublié
-2	Renseigner nom, prenom, email pour réinitialisation de mdp
-3	Est-ce que le nom et le prénom et le mail sont  connus  ? = Non
-4	Pop up Contacter une agence ou annuler ?
-5	Annuler 
-6	Renseigner nom, prenom, email pour demander la réinitialisation de mdp
-7	Est-ce que le nom et le prénom et l'émail sont  connus  ? = Oui
-8	Envoi d'un mail de réinitialisation de mot de passe	 					  HORS SYSTEME
-9		Est-ce que le mail a été reçu ? = Oui    							  HORS SYSTEME
-10		Cliquer sur le lien reçu 											  HORS SYSTEME
-11		Renseigner mail, et mot de passe , et confirmation du mot de passe 	  HORS SYSTEME
-12  Quelle action faire ? = Connexion			  	  						  HORS SYSTEME
-13	Renseigner email et mot de passe           	 							  HORS SYSTEME
- 
- */
