2f0d315fdfbedfa9ea0686bebcf66554d0a916b2
Abderaouf <sahraoui.abderaouf@gmail.com>
2019-09-19 12:18:15 +0200

    Ajout fct Ecran connexion et Applimanager

--- src/main/java/Outils/AppliManager.java
+++ src/main/java/Outils/AppliManager.java
@@ -1,8 +1,11 @@
 package Outils;
 
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 
 public class AppliManager{
 	public static AppiumDriver<MobileElement> driver;
+	public static WebDriverWait wait = new WebDriverWait(driver, 60);
 }
