6516dbe4b41a656df900e0c20fd7d60584211dc4
vg337 <52410625+vg337@users.noreply.github.com>
2019-09-19 17:20:02 +0200

    Maj US 1 Cas 2

--- src/test/java/TestSuite/US1.java
+++ src/test/java/TestSuite/US1.java
@@ -1,8 +1,10 @@
 package TestSuite;
 
+import org.junit.Assert;
 import org.testng.annotations.Test;
 
 import Pages.EcranFirstPage;
+import Pages.EcranAccueil;
 import Pages.EcranConnexion;
 import Pages.EcranMdpOublie;
 
@@ -58,34 +60,71 @@ public class US1 extends TestSuite {
 
 		EcranConnexion eConnex = new EcranConnexion();
 		EcranFirstPage eFirstPage= new EcranFirstPage();
-		EcranReservation eReservation = new EcranReservation();
+		EcranAccueil eAccueil = new EcranAccueil();
 		
 		String adresseEmail = "wcstesteur19@gmail.com";
 		String motDePasse = "testing2019";
 		String motDePasseErrone = "abcdefg99";
 		
+		boolean eAfficheeOK;
+		
 		// 1 Lancer l'application = la page FirstPage est affichée
+		eConnex.verifEcranConnexion();
+		Assert.assertTrue(eAfficheeOK);
+		System.out.println("|---------------------------------------------------|");
+		System.out.println("| STEP : First Page Passed -------------------------|");
+		System.out.println("|---------------------------------------------------|");
+		
 		// 2 Cliquer sur Se Connecter
 		eFirstPage.clickSeConnecter();
+		Assert.assertTrue(eAfficheeOK);
+		System.out.println("|---------------------------------------------------|");
+		System.out.println("| STEP : seConnecter Passed ------------------------|");
+		System.out.println("|---------------------------------------------------|");
 		
 		// 3 Renseigner email et mot de passe erroné
+		
 		eConnex.remplirChampMail(adresseEmail);
 		eConnex.remplirChampMdp(motDePasseErrone);
+		System.out.println("|---------------------------------------------------|");
+		System.out.println("| STEP : Remplissage Mail + MDP Passed -------------|");
+		System.out.println("|---------------------------------------------------|");
 		
 		// 4 Cliquer sur SE CONNECTER
 		eConnex.clickBtnConnexion();
+		System.out.println("|---------------------------------------------------|");
+		System.out.println("| STEP : btn SE CONNECTER Passed -------------------|");
+		System.out.println("|---------------------------------------------------|");
 		
 		// 5 Affichage du message d'erreur "Echec de connexion"
-		eConnex.verifMsgErreurConnexion();
+		eConnex.verifAffichageMsgErreur();
+		Assert.assertTrue(eAfficheeOK);
+		System.out.println("|---------------------------------------------------|");
+		System.out.println("| STEP : Msg Erreur Passed -------------------------|");
+		System.out.println("|---------------------------------------------------|");
 		
 		// 6 Renseigner email et mot de passe
 		eConnex.remplirChampMail(adresseEmail);
 		eConnex.remplirChampMdp(motDePasse);
+		System.out.println("|---------------------------------------------------|");
+		System.out.println("| STEP : Remplissage Mail + MDP Passed -------------|");
+		System.out.println("|---------------------------------------------------|");
 		
 		// 7 Cliquer sur SE CONNECTER
 		eConnex.clickBtnConnexion();
+		System.out.println("|---------------------------------------------------|");
+		System.out.println("| STEP : btn SE CONNECTER Passed -------------------|");
+		System.out.println("|---------------------------------------------------|");
 		
 		// 8 Connexion réussie , page d'accueil  réservation affichée
 		eReservation.verifPageReservationAffichee();
+		Assert.assertTrue(eAfficheeOK);
+		eConnex.clickBtnConnexion();
+		System.out.println("|---------------------------------------------------|");
+		System.out.println("| STEP : CONNEXION Passed --------------------------|");
+		System.out.println("|---------------------------------------------------|");
+		System.out.println("|---------------------------------------------------|");
+		System.out.println("| STEP : FIN US 1 CAS 2 Passed ---------------------|");
+		System.out.println("|---------------------------------------------------|");
 	}
 }
