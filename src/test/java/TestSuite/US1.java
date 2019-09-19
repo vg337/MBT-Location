b8183d406af3f0d863eadc55fbc782aa8c07b48c
Eldakhan <julien.armentia@hotmail.fr>
2019-09-19 11:26:30 +0200

    US1 CDT2

--- src/test/java/TestSuite/US1.java
+++ src/test/java/TestSuite/US1.java
@@ -51,4 +51,40 @@ public class US1 extends TestSuite {
 		eMdpOublie.clickEnvoyer();		
 		
 	}
+	
+	
+	@Test
+	public void connexion() {
+
+		EcranConnexion eConnex = new EcranConnexion();
+		EcranFirstPage eFirstPage= new EcranFirstPage();
+		
+		String adresseEmail = "wcstesteur19@gmail.com";
+		String motDePasse = "testing2019";
+		String motDePasseErrone = "abcdefg99";
+		
+		// Lancer l'application = la page FirstPage est affichée
+		// 2 Cliquer sur Se Connecter
+		eFirstPage.clickSeConnecter();
+		
+		// 3 Renseigner email et mot de passe erroné
+		eConnex.remplirChampMail(adresseEmail);
+		eConnex.remplirChampMdp(motDePasseErrone);
+		
+		// 4 Cliquer sur SE CONNECTER
+		eConnex.clickBtnConnexion();
+		
+		// 5 Affichage du message d'erreur "Echec de connexion"
+		eConnex.verifMsgErreurConnexion();
+		
+		// 6 Renseigner email et mot de passe
+		eConnex.remplirChampMail(adresseEmail);
+		eConnex.remplirChampMdp(motDePasse);
+		
+		// 8 Cliquer sur SE CONNECTER
+		eConnex.clickBtnConnexion();
+		
+		// 9 Connexion réussie , page d'accueil  réservation affichée
+		eConnex.verifPageReservationAffichee();
+	}
 }
