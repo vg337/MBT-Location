c623abe48d71819a91b0a458177d70d0b9db3b74
vg337 <52410625+vg337@users.noreply.github.com>
2019-09-19 12:52:02 +0200

    Maj driver

--- src/main/java/Pages/Réservations/C_recapVehiculeEtChoixPaiement.java
+++ src/main/java/Pages/Réservations/C_recapVehiculeEtChoixPaiement.java
@@ -2,18 +2,22 @@ package Pages.Réservations;
 
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import Outils.AppliManager;
+
 import org.openqa.selenium.By;
 
-public class C_recapVehiculeEtChoixPaiement {
+public class C_recapVehiculeEtChoixPaiement extends AppliManager{
 
 	private By payerMtn = By.xpath("");
 	
 	public void payerMaintenant() {
-		WebDriverWait wait = WebDriverWait(driver, 60);
+		
+		WebDriverWait wait = new WebDriverWait(driver, 60);
 		
 		//Je selectionne le véhicule 1 :
 		wait.until(ExpectedConditions.elementToBeClickable(payerMtn));
-		driver.findElement(payerMtn).click;
+		driver.findElement(payerMtn).click();
 	}
 	
 }
