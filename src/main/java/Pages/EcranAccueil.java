d09f82dba081fcba5c1ecd65b3a685d484c2aa5e
Skibouille <yassine.skiba@gmail.com>
2019-09-19 16:31:13 +0200

    Maj PageAccueil + PageConnexion

--- src/main/java/Pages/EcranAccueil.java
+++ src/main/java/Pages/EcranAccueil.java
@@ -1,15 +1,10 @@
 package Pages;
 
-import java.sql.Driver;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 
 import Outils.AppliManager;
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileElement;
 
 /**
  * Méthode pour la page D'Accueil après première connexion.
@@ -24,6 +19,7 @@ public class EcranAccueil extends AppliManager {
 	private By btnSeConnecter = By.xpath("//*[contains(text(),'SE CONNECTER')]");
 	private By champAgenceDepart = By.xpath("//*[contains(text(),'Indiquez une agence de départ')]");
 	private By textReserverMaintenant = By.xpath("//*[contains(text(),'RESERVER MAINTENANT')]");
+	private boolean visibilitTextReserverMaintenante;
 
 	/*
 	 * Méthode pour cliquer sur "Se Connecter"
@@ -56,16 +52,15 @@ public class EcranAccueil extends AppliManager {
 	public boolean verifEcranAccueil() {
 		
 		try {
-			WebDriverWait wait = new WebDriverWait(driver, 60);
+			wait = new WebDriverWait(driver, 60);
 			wait.until(ExpectedConditions.visibilityOfElementLocated(textReserverMaintenant));
-			boolean visibilitTextReserverMaintenante = driver.findElement(textReserverMaintenant).isDisplayed();
-			return visibilitTextReserverMaintenante;
+			visibilitTextReserverMaintenante = driver.findElement(textReserverMaintenant).isDisplayed();
 
-		} catch (Exception e) {
+		} catch (Exception e) {			
 			System.out.println("Cause is :" + e.getCause());
             System.out.println("Message is : " + e.getMessage());
             e.printStackTrace();
 		}
-		
+		return visibilitTextReserverMaintenante;
 	}
 }
